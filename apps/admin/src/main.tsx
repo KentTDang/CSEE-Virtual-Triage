144b4438d2a6eef29513cdfbdb0775d74377132e
Kent Dang <kenttdang@gmail.com>
2025-10-01 19:49:24 -0400

    [feat]: ✨ Implemented a sidebar for the admin page

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@workspace/ui/globals.css";
 import App from "./App";
+import Layout from "./layout";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Layout>
+      <App />
+    </Layout>
+  </StrictMode>
 );
