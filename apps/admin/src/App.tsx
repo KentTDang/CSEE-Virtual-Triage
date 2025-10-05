6a6eb5863112e616955b916618a5a8cb2b41cddc
corebenj <168385529+corebenj@users.noreply.github.com>
2025-10-05 19:22:43 -0400

    Set up admin auth with Supabase & Implemented Signin/Signout page

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,24 +1,14 @@
 import { useState } from "react";
 import { Button } from "@workspace/ui/components/button";
+import Signup from "./components/Signup"
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="bg-red-500">
-        <Button>Shadcn</Button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Signup />
     </>
   );
 }
--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@workspace/ui/globals.css";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <>
+      <h1 className="text-center pt-4 text-3xl"> 
+        React Supabase Authentication 
+      </h1>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
+    </>
+  </StrictMode>
 );
