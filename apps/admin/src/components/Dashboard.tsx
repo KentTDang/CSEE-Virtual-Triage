0bb32503becdc1d11a3c72ebb3b37b370683bdd0
corebenj <168385529+corebenj@users.noreply.github.com>
2025-10-08 19:37:27 -0400

    FAQ dashboard

--- apps/admin/src/components/Dashboard.tsx
+++ apps/admin/src/components/Dashboard.tsx
@@ -1,12 +1,18 @@
 // import React from 'react'
 import { UserAuth } from '../context/AuthContext.jsx';
 import { useNavigate } from 'react-router-dom';
+// import { supabase } from '../supabaseClient';
+// import { useState, useEffect } from 'react';
+import { Button } from "@workspace/ui/components/button";
+
+// admin user profiles (signout will be on this page/profile management{pass reset, etc})
+// admin uuid's for adding/modifying FAQs
+// FAQ modification backlog that tracks modifications?
+// 
 
 const Dashboard = () => {
   const { session, signOut } = UserAuth();
   const navigate = useNavigate();
-  
-  console.log(session);
 
   const handleSignOut = async(e) => {
     e.preventDefault()
@@ -18,19 +24,35 @@ const Dashboard = () => {
     }
   };
 
+  const handleFAQ = () => {
+    navigate('/FAQboard');
+  }
+
+  const handleChatbot = () => {
+    navigate('/');
+  }
+
   return (
     <div>
-      <h1>Dashboard</h1>
-      <h2>Welcome, {session?.user?.email}</h2>
-      <div>
-        <p 
-          onClick={handleSignOut}
-          className="hover:cursor-pointer border inline-block px-4 py-3 mt-4"
-        >
-          Sign Out
-        </p>
-      </div>
+      <div className = "flex">
+        <h1>Dashboard</h1>
+        <h2>Welcome, {session?.user?.email}</h2>
+          <p 
+            onClick={handleSignOut}
+            className="hover:cursor-pointer border inline-block px-4 py-3 mt-4"
+          >
+            Sign Out
+          </p>
+        </div>
+      <div className= "m-3 gap-2">
 
+        <Button onClick={handleFAQ}>FAQ Management</Button>
+
+        <Button onClick={handleChatbot}>Chatbot</Button>
+        
+        <Button>FAQ Backlog</Button>
+
+      </div>
 
     </div>
   )
