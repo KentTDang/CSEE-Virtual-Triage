159fe2aa0c37e5149c05905b5253d7b0eb57dde7
Kamronbek Rakhimov <156855800+rkamron@users.noreply.github.com>
2025-10-16 01:12:32 -0400

    more

--- chat_backend/ingestion.py
+++ chat_backend/ingestion.py
@@ -1,9 +1,9 @@
-# ingestion.py
 import os
+from bs4 import BeautifulSoup
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,13 +11,12 @@ load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 CHROMA_API_KEY = os.getenv("CHROMA_API_KEY")
 CHROMA_TENANT = os.getenv("CHROMA_TENANT")
-CHROMA_HOST = os.getenv("CHROMA_HOST", "https://api.trychroma.com")
+CHROMA_DATABASE = os.getenv("CHROMA_DATABASE", "development")
 
 URLS = [
-    "https://cs.umbc.edu/",
-    "https://cs.umbc.edu/undergraduate/",
-    "https://cs.umbc.edu/graduate/",
-    "https://cs.umbc.edu/faculty/",
+    "https://www.csee.umbc.edu/",
+    "https://www.csee.umbc.edu/overview/",
+    "https://www.csee.umbc.edu/undergraduate/"
 ]
 
 loader = WebBaseLoader(URLS)
@@ -31,18 +30,13 @@ print(f"Created {len(split_docs)} chunks")
 print("Creating embeddings with Gemini...")
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
-print("Uploading to Chroma Cloud...")
 vectorstore = Chroma.from_documents(
     documents=split_docs,
     embedding=embeddings,
     collection_name="umbc-department",
-    client_settings={
-        "chroma_api_impl": "rest",
-        "chroma_server_host": CHROMA_HOST,
-        "chroma_server_http_port": 443,
-        "chroma_api_key": CHROMA_API_KEY,
-        "chroma_tenant": CHROMA_TENANT,
-    },
+    chroma_cloud_api_key=CHROMA_API_KEY,
+    tenant=CHROMA_TENANT,
+    database=CHROMA_DATABASE
 )
 
 print("Ingestion complete! Data uploaded to Chroma Cloud.")
