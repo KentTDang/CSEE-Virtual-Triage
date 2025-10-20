c49b8b7e771dad7b82fc59313c56ed04dbc713fb
Kent Dang <kenttdang@gmail.com>
2025-10-20 16:16:36 -0400

    [fix]: 🔧 corrected comment and package lock

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -8,24 +8,23 @@ import { AuthContextProvider } from "./context/AuthContext.jsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <>
-      <h1 className="text-center pt-4 text-3xl"> 
-        React Supabase Authentication 
+      <h1 className="text-center pt-4 text-3xl">
+        React Supabase Authentication
       </h1>
       <AuthContextProvider>
         <RouterProvider router={router} />
       </AuthContextProvider>
     </>
-
   </StrictMode>
 );
-        
-<!--         
-import App from "./App";
-import Layout from "./layout";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Layout>
-      <App />
-    </Layout>
- -->
+// import App from "./App";
+// import Layout from "./layout";
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <Layout>
+//       <App />
+//     </Layout>
+//     </StrictMode>
+// );
