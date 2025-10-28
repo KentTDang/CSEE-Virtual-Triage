6e8bdca76661f8555616dc602f474505b2533f80
Kamronbek Rakhimov <156855800+rkamron@users.noreply.github.com>
2025-10-28 00:52:01 -0400

    added components and tested apps/wed/src/App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
-import { Button } from "@workspace/ui/components/button";
+import {
+  Conversation,
+  ConversationContent,
+  ConversationScrollButton,
+} from "../../../packages/ui/src/components/ui/shadcn-io/ai/conversation";
+
+import {
+  Message,
+  MessageContent,
+} from "../../../packages/ui/src/components/ui/shadcn-io/ai/message";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="bg-blue-500">
-        <Button>Shadcn</Button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Conversation className="relative w-full" style={{ height: "500px" }}>
+      <ConversationContent>
+        <Message from={"user"}>
+          <MessageContent>Hi there!</MessageContent>
+        </Message>
+      </ConversationContent>
+      <ConversationScrollButton />
+    </Conversation>
   );
 }
 
