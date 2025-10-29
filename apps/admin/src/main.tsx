3cc0f715a692ef9bc5d0692e37de097650a55fce
Kent <kenttdang@gmail.com>
2025-10-28 22:30:08 -0400

    fix: 🔧created a protected layout

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -7,24 +7,8 @@ import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <>
-      <h1 className="text-center pt-4 text-3xl">
-        React Supabase Authentication
-      </h1>
-      <AuthContextProvider>
-        <RouterProvider router={router} />
-      </AuthContextProvider>
-    </>
+    <AuthContextProvider>
+      <RouterProvider router={router} />
+    </AuthContextProvider>
   </StrictMode>
 );
-
-// import App from "./App";
-// import Layout from "./layout";
-
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <Layout>
-//       <App />
-//     </Layout>
-//     </StrictMode>
-// );
--- apps/admin/src/router.tsx
+++ apps/admin/src/router.tsx
@@ -1,29 +1,27 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./App.js";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import Signup from "./components/Signup.js";
 import Signin from "./components/Signin.js";
 import Dashboard from "./components/Dashboard.js";
 import PrivateRoute from "./components/PrivateRoute.js";
 import FAQboard from "./components/FAQboard.js";
 
+function PrivateLayout() {
+    return (
+        <PrivateRoute>
+            <Outlet />
+        </PrivateRoute>
+    )
+}
+
 export const router = createBrowserRouter([
-    { path: "/", element: <App />},
+    { path: "/", element: <Signin />},
     { path: "/signup", element: <Signup />},
     { path: "/signin", element: <Signin />},
-    { 
-        path: "/faq", 
-        element: (
-            <PrivateRoute>
-                <FAQboard />
-            </PrivateRoute>
-        ), 
-    },
-    { 
-        path: "/dashboard", 
-        element: (
-            <PrivateRoute>
-                <Dashboard />
-            </PrivateRoute>
-        ),
+    {
+        element: <PrivateLayout />,
+        children: [
+            {path: "/faq", element: <FAQboard />},
+            {path: "/dashboard", element: <Dashboard />},
+        ]
     },
 ]);
