8ba26768f106a756b869bf605b5aa36e2aec4037
Kent Dang <kenttdang@gmail.com>
2025-12-03 12:44:53 -0500

    fix(web): 🔧 single page layout

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@workspace/ui/globals.css";
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./AppRoutes";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
